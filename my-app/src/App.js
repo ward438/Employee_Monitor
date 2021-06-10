d367004f22b03ab75e28e3c06d76d8029f1de6f6
ward438 <75048548+ward438@users.noreply.github.com>
2021-06-10 17:55:21 -0500

    fixed issue with axios call and passed data to Button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
 import { ListGroupItem, ListGroup, Button } from 'react-bootstrap';
-import React, { useState } from "react";
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 
 function Card(props) {
@@ -11,73 +11,29 @@ function Card(props) {
       <div>
         Name: {props.employee.name.title}. {props.employee.name.first} {props.employee.name.last}
       </div>
+
+      <div>
+        Name:       
+        {props.employee.name.first} {props.employee.name.last}
+      </div>
     </div>
   )
+};
+
+const getEmployees = () => {    
+  return axios.get('https://randomuser.me/api/')
+    .then(response => {
+      console.log(response.data.results)
+      return response.data;
+    })             
 }
-function GetEmployees() {
-  return {
-    results: [
-      {
-        gender: "male",
-        name: {
-          title: "mr",
-          first: "brad",
-          last: "gibson"
-        },
-        location: {
-          street: "9278 new road",
-          city: "kilcoole",
-          state: "waterford",
-          postcode: "93027",
-          coordinates: {
-            latitude: "20.9267",
-            longitude: "-7.9310"
-          },
-          timezone: {
-            offset: "-3:30",
-            description: "Newfoundland"
-          }
-        },
-        email: "brad.gibson@example.com",
-        login: {
-          uuid: "155e77ee-ba6d-486f-95ce-0e0c0fb4b919",
-          username: "silverswan131",
-          password: "firewall",
-          salt: "TQA1Gz7x",
-          md5: "dc523cb313b63dfe5be2140b0c05b3bc",
-          sha1: "7a4aa07d1bedcc6bcf4b7f8856643492c191540d",
-          sha256: "74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480"
-        },
-        dob: {
-          date: "1993-07-20T09:44:18.674Z",
-          age: 26
-        },
-        registered: {
-          date: "2002-05-21T10:59:49.966Z",
-          age: 17
-        },
-        phone: "011-962-7516",
-        cell: "081-454-0666",
-        id: {
-          name: "PPS",
-          value: "0390511T"
-        },
-        picture: {
-          large: "https://randomuser.me/api/portraits/men/75.jpg",
-          medium: "https://randomuser.me/api/portraits/med/men/75.jpg",
-          thumbnail: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-        },
-        nat: "IE"
-      }
-    ],
-    info: {
-      seed: "fea8be3e64777240",
-      results: 1,
-      page: 1,
-      version: "1.3"
-    }
-  }
-}
+
+
+
+
+
+
+
 
 function App() {
   const [employees, setEmployeesState] = useState({
@@ -89,15 +45,15 @@ function App() {
     <div>
       <Button onClick={() => {
         console.log('mock - api call button')
-        setEmployeesState(GetEmployees());
+        getEmployees().then(employees=>{
+          setEmployeesState(employees);
+        });        
       }}>Get Employees</Button>
-      {employees.results.map(employee => <Card key={employee.email} employee={employee} setEmployeesState={setEmployeesState} />)}
+      {/* UNCOMMENT THIS OUT ONCE YOU FIGURE OUT HOW TO MOVE THE GetEmpoloyees() is fixed */}
+      {employees.results.map(employee => <Card key={employee.name} employee={employee} setEmployeesState={setEmployeesState} />)}
     </div>
   );
 
 };
-
-
-
 
 export default App;
