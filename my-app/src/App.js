ba47b60d31c795c33ff51e15a43cf1a20e618e7f
ward438 <75048548+ward438@users.noreply.github.com>
2021-06-10 22:46:16 -0500

    styling, arranging data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,39 +1,72 @@
 
 import './App.css';
 import { ListGroupItem, ListGroup, Button } from 'react-bootstrap';
-import React, { useState } from 'react';
+import React, { useState, component } from 'react';
 import axios from 'axios';
+import { Linking } from 'react-native'
 
 
-function Card(props) {
-  return (
-    <div className="card">
+
+
+function Card(props) {  
+  let logo = `${props.employee.picture.medium}`
+  let email = `${props.employee.email}`
+  return (    
+    <table className="card" style={{width: '45rem'}}>
       <div>
-        Name: {props.employee.name.title}. {props.employee.name.first} {props.employee.name.last}
-      </div>
-
-      <div>
-        Name:       
-        {props.employee.name.first} {props.employee.name.last}
-      </div>
-    </div>
+        <tr>
+          <img src={logo} alt="Photo"/>         
+        </tr>        
+        <td>Name:&nbsp;            
+          {props.employee.name.title}.&nbsp;       
+          {props.employee.name.first}&nbsp;           
+          {props.employee.name.last}                    
+        </td>             
+        <tr>
+          <td>Age: {props.employee.dob.age}</td>                 
+        </tr>
+        <tr>
+          <td>
+            Location: {props.employee.location.city},{props.employee.location.state}
+          </td>
+        </tr>
+        <tr>
+          <td>            
+            <a href={email} alt="Email">{props.employee.email}</a>
+            {/* <Button onPress={() => Linking.openURL('mailto:support@example.com') }
+            title="support@example.com"/>
+                      */}
+          </td>
+        </tr>
+        <tr>
+          <td>
+           Cell:&nbsp; 
+            {props.employee.cell}
+          </td>
+          <td>
+           LAN Line:&nbsp; 
+            {props.employee.phone}
+          </td>
+        </tr>
+        
+      </div>     
+    </table>
+    
+    
   )
 };
 
-const getEmployees = () => {    
-  return axios.get('https://randomuser.me/api/')
+const getEmployees = () => {
+
+  // let pageNum = [`${'1'}`];   
+  
+  return axios.get(`https://randomuser.me/api/?results=100`)
+  // return axios.get(`https://randomuser.me/api/?page=${pageNum}&results=10&seed=abc`)
     .then(response => {
       console.log(response.data.results)
       return response.data;
     })             
 }
-
-
-
-
-
-
-
 
 function App() {
   const [employees, setEmployeesState] = useState({
@@ -49,9 +82,11 @@ function App() {
           setEmployeesState(employees);
         });        
       }}>Get Employees</Button>
+      
       {/* UNCOMMENT THIS OUT ONCE YOU FIGURE OUT HOW TO MOVE THE GetEmpoloyees() is fixed */}
       {employees.results.map(employee => <Card key={employee.name} employee={employee} setEmployeesState={setEmployeesState} />)}
     </div>
+    
   );
 
 };
